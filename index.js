2fa6b9f99cbac275637b760694aebacef82791a5
wataru5354 <setsunan76253@gmail.com>
2022-05-02 15:33:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+function pullDown(){
+
+  const pullDownButton = document.getElementById("lists")
+  const pullDownParents = document.getElementById("pull-down")
+  const pullDownChild = document.querySelectorAll(".pull-down-list")
+  const currentList = document.getElementById("current-list")
+
+  pullDownButton.addEventListener('mouseover', function(){
+    this.setAttribute("style","background-color:#FFBEDA;")
+  })
+  pullDownButton.addEventListener('mouseout',function(){
+    this.removeAttribute("style","background-color:#FFBEDA;")
+  })
+  pullDownButton.addEventListener('click',function(){
+    if(pullDownParents.getAttribute("style") == "display: block;"){
+      pullDownParents.removeAttribute("style","display: block;")
+    } else {
+      pullDownParents.setAttribute("style","display: block;")
+    }
+  })
+  pullDownChild.forEach(function(list){
+    list.addEventListener('click',function(){
+      const value = list.innerHTML
+      console.log(value)
+      currentList.innerHTML = value
+    })
+  })
+}
+
+window.addEventListener('load',pullDown)
